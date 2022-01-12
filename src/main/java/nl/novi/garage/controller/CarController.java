e6e90cfb806aba27e20bea6f9808f949e14eca3e
Bwiggemans <b.wiggemans@home.nl>
2022-01-12 12:49:03 +0100

    Lombok dependency en tijdelijke lijst cars is verwijdert

--- src/main/java/nl/novi/garage/controller/CarController.java
+++ src/main/java/nl/novi/garage/controller/CarController.java
@@ -15,40 +15,6 @@ import java.util.List;
 @RestController
 public class CarController {
 
-    //Attribute
-    private List<Car> cars = new ArrayList<>();
-
-    //Constructor
-    public CarController() {
-        Car car1 = new Car();
-        car1.setBrand("Kia");
-        car1.setModel("Sportage");
-        car1.setFuel("diesel");
-        car1.setTransmission("Automatic");
-        car1.setYear(2019);
-        car1.setMileage(19500.5);
-        cars.add(car1);
-
-        Car car2 = new Car();
-        car2.setBrand("Kia");
-        car2.setModel("Forte");
-        car2.setFuel("petrol");
-        car2.setTransmission("Manual");
-        car2.setYear(2016);
-        car2.setMileage(75095.7);
-        cars.add(car2);
-
-        Car car3 = new Car();
-        car3.setBrand("Kia");
-        car3.setModel("Niro");
-        car3.setFuel("petrol");
-        car3.setTransmission("Manual");
-        car3.setYear(2018);
-        car3.setMileage(35155.7);
-        cars.add(car3);
-
-    }
-
     @Autowired
     private CarRepository carRepository;
 
@@ -133,7 +99,6 @@ public class CarController {
         if ((car.getMileage()) != (existingCar.getMileage())){
             existingCar.setMileage(car.getMileage());
         }
-
         carRepository.save(existingCar);
 
         return ResponseEntity.noContent().build(); // 200 code wordt teruggegeven
--- src/main/java/nl/novi/garage/model/Car.java
+++ src/main/java/nl/novi/garage/model/Car.java
@@ -1,7 +1,10 @@
 package nl.novi.garage.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 @Table(name = "cars")
 public class Car {
@@ -20,61 +23,6 @@ public class Car {
 
     //Constructor not necessary
 
-    //Getters and setters
+    //Getters and setters vervallen i.v.m. toevoegen Lombok dependency
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getFuel() {
-        return fuel;
-    }
-
-    public void setFuel(String fuel) {
-        this.fuel = fuel;
-    }
-
-    public String getTransmission() {
-        return transmission;
-    }
-
-    public void setTransmission(String transmission) {
-        this.transmission = transmission;
-    }
-
-    public Integer getYear() {
-        return year;
-    }
-
-    public void setYear(Integer year) {
-        this.year = year;
-    }
-
-    public double getMileage() {
-        return mileage;
-    }
-
-    public void setMileage(double mileage) {
-        this.mileage = mileage;
-    }
 }
