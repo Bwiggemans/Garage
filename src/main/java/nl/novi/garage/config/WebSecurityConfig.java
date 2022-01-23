9bf8b4af7e359d7b33526bd3a4f78d44da73e748
Bwiggemans <b.wiggemans@home.nl>
2022-01-23 16:19:37 +0100

    Security jwt token genereren geimplementeerd

--- src/main/java/nl/novi/garage/config/WebSecurityConfig.java
+++ src/main/java/nl/novi/garage/config/WebSecurityConfig.java
@@ -1,15 +1,20 @@
 package nl.novi.garage.config;
 
+import nl.novi.garage.security.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
 
@@ -19,32 +24,41 @@ import static org.springframework.http.HttpMethod.PATCH;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
     private DataSource dataSource;
 
     @Autowired
-    WebSecurityConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+    private JwtRequestFilter jwtRequestFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    //Define roles and users authentication
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 
         auth.jdbcAuthentication().dataSource(dataSource)
                 .usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username=?")
                 .authoritiesByUsernameQuery("SELECT username, authority FROM authorities AS a WHERE username=?");
+
     }
 
-    // Secure the endpoins with HTTP Basic authentication
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        //HTTP settings Basic authentication
         http
                 .httpBasic()
                 .and()
@@ -59,7 +73,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET,"goodbye").permitAll()
                 .anyRequest().permitAll()
                 .and()
+                .cors()
+                .and()
                 .csrf().disable()
-                .formLogin().disable();
+                .formLogin().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
     }
+
 }
