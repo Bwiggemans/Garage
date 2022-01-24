f793286d43be29de302d30734a7ffc4823e4036e
Bwiggemans <b.wiggemans@home.nl>
2022-01-24 09:54:31 +0100

    CORS configuration toegevoegd deze werkt

--- src/main/java/nl/novi/garage/security/JwtUtil.java
+++ src/main/java/nl/novi/garage/security/JwtUtil.java
@@ -43,8 +43,8 @@ public class JwtUtil {
     }
 
     private String createToken(Map<String, Object> claims, String subject) {
-//        long validPeriod = 1000 * 60 * 60 * 24 * 10;   // 10 days in ms
-        long validPeriod = 1000 * 60;   // 1 min
+        long validPeriod = 1000 * 60 * 60 * 24 * 365;   // 10 days in ms
+//        long validPeriod = 1000 * 60;   // 1 min
         long currentTime = System.currentTimeMillis();
         return Jwts.builder()
                 .setClaims(claims)
