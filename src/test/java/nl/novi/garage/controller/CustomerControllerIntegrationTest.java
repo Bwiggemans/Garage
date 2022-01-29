2af5cc4cbf1e4fbbb51fe901687d1ee75beced24
Bwiggemans <b.wiggemans@home.nl>
2022-01-29 14:22:21 +0100

    Controller Test uitgebreid

---
+++ src/test/java/nl/novi/garage/controller/CustomerControllerIntegrationTest.java
@@ -0,0 +1,40 @@
+package nl.novi.garage.controller;
+
+import nl.novi.garage.GarageApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = GarageApplication.class)
+@AutoConfigureMockMvc
+@EnableConfigurationProperties
+//@WithMockUser(username = "admin", roles = {"ADMIN"})
+public class CustomerControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturn200ForEndpointCustomers() throws Exception {
+        mockMvc.perform(get("/customers").with(user("user").roles("USER")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReturnJson() throws Exception {
+
+        mockMvc.perform(get("/customers").contentType(MediaType.APPLICATION_JSON).with(user("user").roles("USER")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+}
---
+++ src/test/java/nl/novi/garage/controller/UserControllerIntegrationTest.java
@@ -0,0 +1,47 @@
+package nl.novi.garage.controller;
+
+import nl.novi.garage.GarageApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = GarageApplication.class)
+@AutoConfigureMockMvc
+@EnableConfigurationProperties
+//@WithMockUser(username = "admin", roles = {"ADMIN"})
+public class UserControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturn200ForEndpointUsersAdmin() throws Exception {
+        mockMvc.perform(get("/users").with(user("admin").roles("ADMIN")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReturn200ForEndpointUsersUsers() throws Exception {
+        mockMvc.perform(get("/users").with(user("users").roles("ADMIN")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReturnJson() throws Exception {
+
+        mockMvc.perform(get("/users").contentType(MediaType.APPLICATION_JSON).with(user("admin").roles("ADMIN")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+
+}
