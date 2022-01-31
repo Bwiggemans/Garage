7d578e5fbd53c923750cfae60aec8f7524df0a7d
Bwiggemans <b.wiggemans@home.nl>
2022-01-31 13:12:49 +0100

    CarInspectionRepairController en CarInspectionRepairShopController toegevoegd

--- src/main/java/nl/novi/garage/controller/CarInspectionRepairController.java
+++ src/main/java/nl/novi/garage/controller/CarInspectionRepairController.java
@@ -1,4 +1,50 @@
 package nl.novi.garage.controller;
 
+import nl.novi.garage.model.CarInspectionRepair;
+import nl.novi.garage.service.CarInspectionRepairService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/v1")
 public class CarInspectionRepairController {
+
+    @Autowired
+    private CarInspectionRepairService carInspectionRepairService;
+
+    @GetMapping(value = "/cars_inspection_repair")
+    public ResponseEntity<?> getCarInspectionRepairs() {
+        List<CarInspectionRepair> carInspectionRepairs = carInspectionRepairService.getAllCarInspectionRepairs();
+        return ResponseEntity.ok(carInspectionRepairs);
+    }
+
+    @GetMapping(value = "/cars_inspection_repair/{id}")
+    public ResponseEntity<?> getCarInspectionRepair(@PathVariable("id") long id) {
+        return ResponseEntity.ok(carInspectionRepairService.getCarInspectionRepair(id));
+    }
+
+    @PostMapping(value = "/cars_inspection_repair")
+    public ResponseEntity<?> createCarInspectionRepair(@RequestBody CarInspectionRepair carInspectionRepair) {
+        long newId = carInspectionRepairService.save(carInspectionRepair);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newId)
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping(value = "/cars_inspection_repair/{id}")
+    public ResponseEntity<?> deleteCarInspectionRepair(@PathVariable("id") long id) {
+        carInspectionRepairService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/nl/novi/garage/controller/CarInspectionRepairShopController.java
+++ src/main/java/nl/novi/garage/controller/CarInspectionRepairShopController.java
@@ -1,4 +1,51 @@
 package nl.novi.garage.controller;
 
+import nl.novi.garage.model.CarInspectionRepairShop;
+import nl.novi.garage.service.CarInspectionRepairShopService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/v1")
 public class CarInspectionRepairShopController {
+
+    @Autowired
+    private CarInspectionRepairShopService carInspectionRepairShopService;
+
+    @GetMapping(value = "/cars_repair_inspection_shop")
+    public ResponseEntity<?> getCarInspectionRepairShops() {
+        List<CarInspectionRepairShop> carInspectionRepairShops = carInspectionRepairShopService.getAllCarInspectionRepairShops();
+        return ResponseEntity.ok(carInspectionRepairShops);
+    }
+
+    @GetMapping(value = "/cars_repair_inspection_shop/{id}")
+    public ResponseEntity<?> getCarInspectionRepairShop(@PathVariable("id") long id) {
+        return ResponseEntity.ok(carInspectionRepairShopService.getCarInspectionRepairShop(id));
+    }
+
+    @PostMapping(value = "/cars_repair_inspection_shop")
+    public ResponseEntity<?> createCarInspectionRepairShop(@RequestBody CarInspectionRepairShop carInspectionRepairShop) {
+        long newId = carInspectionRepairShopService.save(carInspectionRepairShop);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newId)
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping(value = "/cars_repair_inspection_shop/{id}")
+    public ResponseEntity<?> deleteCarInspectionRepairShop(@PathVariable("id") long id) {
+        carInspectionRepairShopService.deleteById(id);
+        return new ResponseEntity<>("CarInspectionRepairShop deleted", HttpStatus.OK);
+    }
+
 }
